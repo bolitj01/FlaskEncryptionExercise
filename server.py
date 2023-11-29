5fd585e231afece05d07540ac5acfb7e6125877a
Thomas Bolinger <bolitj01@pfw.edu>
2023-11-29 17:21:46 -0500

    key generator script, user print function, additional rest tests

--- server.py
+++ server.py
@@ -1,11 +1,16 @@
 from flask import Flask, request, jsonify
 from cryptography.fernet import Fernet
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+load_dotenv()
+
 # Generate a key for encryption
 # In a production application, save this in a secure location
-key = Fernet.generate_key()
+# Use the key from the environment variable
+key = os.getenv("ENCRYPTION_KEY").encode()
 cipher_suite = Fernet(key)
 
 @app.route('/signup', methods=['POST'])
@@ -24,10 +29,27 @@ def signup():
         with open('users.txt', 'a') as file:
             file.write(f'{encrypted_username.decode()} {encrypted_password.decode()}\n')
 
+        # Decrypt and print all user data from the file
+        print_users()
+
         return jsonify({'status': 'success'}), 200
 
     except Exception as e:
         return jsonify({'error': str(e)}), 400
+
+def print_users():
+    print('User List:')
+    try:
+        with open('users.txt', 'r') as file:
+            for line in file:
+                encrypted_username, encrypted_password = line.strip().split()
+                decrypted_username = cipher_suite.decrypt(encrypted_username.encode()).decode()
+                decrypted_password = cipher_suite.decrypt(encrypted_password.encode()).decode()
+                print(f'Username: {decrypted_username}, Password: {decrypted_password}')
+    except FileNotFoundError:
+        print("The 'users.txt' file was not found.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == '__main__':
     app.run(debug=True)
